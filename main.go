3466351af0031917a0c6193b2aa7fe5334951629
Jesse Adelman <879972+boldandbusted@users.noreply.github.com>
2023-03-14 13:57:14 -0400

    Change to brown

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	myFigure := figure.NewColorFigure("boldandbusted is Awesome!!!", "larry3d", "yellow", true)
+	myFigure := figure.NewColorFigure("boldandbusted is Awesome!!!", "larry3d", "brown", true)
 	myFigure.Print()
 
 	if secret := os.Getenv("SECRET"); secret != "" {
